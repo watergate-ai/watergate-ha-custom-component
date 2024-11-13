c3b9b5bd430582117fa10880f2182ea971dc2835
Kev Willers <kev@thewillers.com>
2024-11-13 01:28:49 +0000

    Update __init__.py

--- custom_components/watergate/__init__.py
+++ custom_components/watergate/__init__.py
@@ -101,14 +101,15 @@ def get_webhook_handler(
             coordinator_data.state.valve_state = data.state
 
         if body_type == WEBHOOK_TELEMETRY_TYPE:
-            # Ensure 'errors' is a dictionary, defaulting to empty if not present
-            errors = getattr(data, "errors", {})
+            # Ensure 'errors' is a dictionary, defaulting to empty if 'data' or 'data.errors' is None
+            errors = getattr(data, "errors", {}) if data else {}
 
             # Set telemetry flow, pressure, and temperature, with None as default if there's an error
             coordinator_data.telemetry.flow = data.flow if errors.get("flow") is None else None
             coordinator_data.telemetry.pressure = data.pressure if errors.get("pressure") is None else None
             coordinator_data.telemetry.water_temperature = data.temperature if errors.get("temperature") is None else None
 
+        
         if body_type == WEBHOOK_WIFI_CHANGED_TYPE:
             coordinator_data.networking.ip = data.ip
             coordinator_data.networking.gateway = data.gateway
